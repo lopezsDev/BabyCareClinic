a9511c9e6138e44b450328f8c0bd2723f708d036
LopezS7 <samirisraellopez21@gmail.com>
2024-04-12 10:35:05 -0600

    Documentacion de la clase Model de paciente, para un mejor entendimiento de la clase.

--- principal/src/main/java/com/project/principal/models/PacienteModel.java
+++ principal/src/main/java/com/project/principal/models/PacienteModel.java
@@ -4,31 +4,56 @@ import java.sql.Date;
 
 import jakarta.persistence.*;
 import lombok.Data;
+
+/**
+ * This class represents a patient entity.
+ */
 @Data
 @Entity
 @Table(name = "Pacientes")
 public class PacienteModel {
 
+    /**
+     * The unique identifier of the patient.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
+    /**
+     * The patient's first name.
+     */
     @Column(name = "nombre")
     private String nombre;
 
+    /**
+     * The patient's last name.
+     */
     @Column(name = "apellido")
     private String apellido;
 
+    /**
+     * The patient's date of birth.
+     */
     @Column(name = "fechaNacimiento")
     private Date fechaNacimiento;
 
+    /**
+     * The patient's gender.
+     */
     @Column(name = "genero")
     private String genero;
 
+    /**
+     * The patient's address.
+     */
     @Column(name = "direccion")
     private String direccion;
 
+    /**
+     * The patient's identification number.
+     */
     @Column(name = "numeroIdentificacion")
     private String numeroIdentificacion;
 }
