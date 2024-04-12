184e26d33809719333b0799807cfa0b1f7a2ab93
LopezS7 <samirisraellopez21@gmail.com>
2024-04-12 10:36:52 -0600

    Documentación de la repositorio del paciente, para un mejor entendimiento a la hora de leer la documentación.

--- principal/src/main/java/com/project/principal/repository/PacienteRepository.java
+++ principal/src/main/java/com/project/principal/repository/PacienteRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.principal.models.PacienteModel;
 
+/**
+ * This interface defines a repository for accessing and managing PacienteModel entities.
+ */
 @Repository
 public interface PacienteRepository extends JpaRepository<PacienteModel, Long> {
 }
