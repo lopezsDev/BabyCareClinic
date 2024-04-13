606902adf338bf57fd2b1532cab7f438264ac490
LopezS7 <samirisraellopez21@gmail.com>
2024-04-13 08:02:11 -0600

    Se agrega la documentación de la clase CitaMedicaRepository

--- principal/src/main/java/com/project/principal/repository/CitaMedicaRepository.java
+++ principal/src/main/java/com/project/principal/repository/CitaMedicaRepository.java
@@ -3,8 +3,15 @@ package com.project.principal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.project.principal.models.CitaMedicaModel;
-
+/**
+ * Repository interface for managing medical appointments.
+ * This interface extends JpaRepository, providing basic CRUD operations for the CitaMedicaModel entity.
+ *
+ * @author [lopezs.dev]
+ * @version [1.1]
+ */
 @Repository
 public interface CitaMedicaRepository extends JpaRepository<CitaMedicaModel, Long> {
 }
+
 
