f9dc8a834f6a87c357f05d083aec8b08753c892b
LopezS7 <samirisraellopez21@gmail.com>
2024-06-16 23:39:44 -0600

    Se agrega la clase que maneja las exepciones de tipo de autenticacion de la app

---
+++ backend/src/main/java/com/project/principal/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.project.principal.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+/**
+ * Esta clase maneja los diferentes exepciones de tipo autenticacion de nuestra app
+ * */
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
