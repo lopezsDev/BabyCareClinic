3d9f7434e1fee24ec224424ef9223d7fe5124b21
LopezS7 <samirisraellopez21@gmail.com>
2024-06-17 01:25:24 -0600

    Creacion de DTO para la correcta transferencia de datos y proteccion de los mismos

---
+++ backend/src/main/java/com/project/principal/dto/LoginDTO.java
@@ -0,0 +1,6 @@
+package com.project.principal.dto;
+
+public class LoginDTO {
+    private String username;
+    private String password;
+}
---
+++ backend/src/main/java/com/project/principal/dto/RegistroDTO.java
@@ -0,0 +1,7 @@
+package com.project.principal.dto;
+
+public class RegistroDTO {
+
+    private String username;
+    private String password;
+}
